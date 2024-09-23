a774ea0c7278eb72e78d54f74fc547e9b5c17e68
Gopikrishna1033 <venkatagopikrishna007@gmail.com>
2024-09-23 18:08:15 +0530

    task

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import FastfoodIcon from '@mui/icons-material/Fastfood';
-import {AppBar, Box, IconButton, Toolbar, Typography,Divider} from "@mui/material"
+import {AppBar, Box, IconButton, Toolbar, Typography,Divider, Drawer} from "@mui/material"
 import { Link } from 'react-router-dom';
 import "../../Styles/HeaderStyles.css"
 import MenuIcon from '@mui/icons-material/Menu';
@@ -12,14 +12,20 @@ const Header = () => {
   }
   //menu drawer
   const drawer = (
-    <Box>
-      <Typography color={'goldenrod'} variant='h6' component={"div"} sx={{flexGrow:1,my:2}}>
+    <Box onClick={handleDrawerToggle} sx={{textAlign:"Center"}} width={240}>
+      <Typography color={'goldenrod'} variant='h6' component={"div"} >
+      <FastfoodIcon/>
+        My Restaurant
       </Typography>
-      <Divider>
-        <ul>
-          <li></li>
+      <Divider></Divider>
+        <ul className='mobile-navigation'>
+          
+          <li><Link to={"/"}>Home</Link></li>
+          <li><Link to={"/about"}>About</Link></li>
+          <li><Link to={"/contact"}>Contact</Link></li>
+          <li><Link to={"/menu"}>Menu</Link></li>
         </ul>
-      </Divider>
+
     </Box>
   )
   return (
@@ -27,7 +33,7 @@ const Header = () => {
        <Box>
           <AppBar component={"nav"} sx={{background:"black"}}>
             <Toolbar>
-              <IconButton color='inherit' aria-label='open drawer' edge='start' sx={{mr:2,display:{sm:"none"}}}>
+              <IconButton color='inherit' aria-label='open drawer' edge='start' sx={{mr:2,display:{sm:"none"}}} onClick={handleDrawerToggle}>
                 <MenuIcon/>
               </IconButton>
                <Typography color={'goldenrod'} variant='h6' component={"div"} sx={{flexGrow:1}}>
@@ -44,6 +50,17 @@ const Header = () => {
                 </Box>
             </Toolbar>
           </AppBar>
+          <Box component={"nav"}>
+            <Drawer variant='temporary' open={mobileOpen} onClose={handleDrawerToggle}sx={{display:{xs:'block', sm:'none'},"&.MuiDrawer-paper":{boxSizing:"border-box",
+              
+            },}} >
+              {drawer}
+            </Drawer>
+          </Box>
+          <Box >
+          <Toolbar/>
+          </Box>
+          
        </Box>
     </div>
   )
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import Layout from '../Components/Layout/Layout'
-
+import { Link } from 'react-router-dom'
+import Banner from "../images/banner.jpeg"
+import "../Styles/HomeStyle.css"
 const Home = () => {
   return (
     <Layout>
-        <h1>Home Page</h1>
+        <div className="home" style={{backgroundImage:`url(${Banner})`}}>
+          <div className="headerContainer">
+            <h1>Food Website</h1>
+            <p>Best Food In India</p>
+            <Link to={"/menu"}>
+              <button>Order Now</button>
+            </Link>
+          </div>
+        </div>
     </Layout>
   )
 }
